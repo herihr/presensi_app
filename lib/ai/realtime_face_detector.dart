1ef8a9c1f824d606a44026d6477f92d24df4fd85
YosephKurubinganBekayo <bekayokurnia@gmail.com>
2026-05-15 16:44:47 +0800

    mh

--- lib/ai/realtime_face_detector.dart
+++ lib/ai/realtime_face_detector.dart
@@ -1,7 +1,6 @@
 import 'dart:math' as math;
 import 'dart:async';
 import 'dart:isolate';
-import 'dart:typed_data';
 
 import 'package:camera/camera.dart';
 import 'package:flutter/foundation.dart';
